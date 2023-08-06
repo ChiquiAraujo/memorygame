f6916b94179c7f0a8ffb4c954b558f063a24ace2
Chiqui <chiqui@MacBook-Pro.local>
2023-08-06 20:54:07 +0200

    Reorganizar archivos

---
+++ js/aciertoError.js
@@ -0,0 +1,12 @@
+
+function acierto (lasTarjetas) {
+    lasTarjetas.forEach(function(elemento){
+        elemento.classList.add("acertada");
+    });
+}
+
+function error (lasTarjetas) {
+    lasTarjetas.forEach(function(elemento){
+        elemento.classList.remove("descubierta");
+    });
+}
